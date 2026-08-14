92239a5561363271ba00b65611a109f371dc3d5e
Guthrie-Center <gc@springbranchisd.com>
2026-08-14 04:34:47 -0500

    Fix config.js line breaks

--- config.js
+++ config.js
@@ -1,1 +1,5 @@
-window.GUTHRIE_CLOCK_CONFIG = {\n  API_URL: "https://script.google.com/macros/s/AKfycbyYNd5UMN7T8a6a3E31popIv-xlK02o7t8YrY9lE3KPVoX8AbEKe9J7RLK1rTOveYyF1g/exec",\n  SCHOOL_TIME_ZONE: "America/Chicago",\n  DUPLICATE_SCAN_SECONDS: 8\n};\n
+window.GUTHRIE_CLOCK_CONFIG = {
+  API_URL: "https://script.google.com/macros/s/AKfycbyYNd5UMN7T8a6a3E31popIv-xlK02o7t8YrY9lE3KPVoX8AbEKe9J7RLK1rTOveYyF1g/exec",
+  SCHOOL_TIME_ZONE: "America/Chicago",
+  DUPLICATE_SCAN_SECONDS: 8
+};
